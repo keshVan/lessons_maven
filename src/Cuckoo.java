5fec00deddb3e6ac9b9cfddefe633d0d338aeed8
keshVan <92533538+keshVan@users.noreply.github.com>
2024-10-24 10:27:27 +0400

    Update Cuckoo.java

--- src/Cuckoo.java
+++ src/Cuckoo.java
@@ -2,14 +2,16 @@ import java.util.Random;
 
 public class Cuckoo extends Bird {
 
-    public Cuckoo(){}
+    public Cuckoo() {
+        super("ку-ку");
+    }
 
     public void sing() {
         Random r = new Random();
         String out = "";
 
         for (int i = 0; i < r.nextInt(10) + 1; i++)
-            out += "ку-ку ";
+            out += getText() + " ";
 
         System.out.println(out.trim());
     }
