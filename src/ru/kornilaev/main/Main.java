e4f845ae01e8a903741aeda90946e840ed65ce7c
keshVan <92533538+keshVan@users.noreply.github.com>
2024-12-04 12:58:19 +0400

    Add files via upload

--- src/ru/kornilaev/main/Main.java
+++ src/ru/kornilaev/main/Main.java
@@ -5,18 +5,15 @@ import ru.kornilaev.math.Fraction;
 import ru.kornilaev.geometry.*;
 
 import java.util.*;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
 
-        Line<Point3D> line = new Line<>(new Point3D(1, 1, 1), new Point3D(2, 2, 2));
+        Line<Point3D> line = Line.of(new Point3D(1, 1, 1), new Point3D(2, 2, 2));
         System.out.println(line);
         shiftLine(line);
         System.out.println(line);
-        Line<Point> line2 = new Line<>(new Point(-5, 1), new Point(1, 2));
+        Line<Point> line2 = Line.of(new Point(-5, 1), new Point(1, 2));
         System.out.println(line2);
         shiftLine(line2);
         System.out.println(line2);
@@ -45,16 +42,16 @@ public class Main {
         System.out.println("6.3.1-------------------------------------");
 
         List<String> stringList = new ArrayList<>(List.of("qwerty", "asdfg", "zx"));
-        List<Integer> stringLengths = func(stringList, a -> a.length());
+        List<Integer> stringLengths = map(stringList, a -> a.length());
         System.out.println(stringLengths);
 
         List<Integer> integerList = new ArrayList<>(List.of(1, -3, 7));
-        List<Integer> newIntegerList = func(integerList, a -> Math.abs(a));
+        List<Integer> newIntegerList = map(integerList, a -> Math.abs(a));
         System.out.println(newIntegerList);
-        int[] arr = new int[]{1, 2, 3, 4};
+
 
         List<int[]> listOfArrays = new ArrayList<>(List.of(new int[]{1, 5, 7}, new int[]{23, -5, 1}, new int[]{-9, 1, 3}));
-        List<Integer> newListOfArrays = func(listOfArrays, (int[] a) -> {
+        List<Integer> newListOfArrays = map(listOfArrays, (a) -> {
             int max = a[0];
             for (int i = 0; i < a.length; i++)
                 if (max < a[i]) max = a[i];
@@ -79,7 +76,7 @@ public class Main {
         System.out.println(newListOfArrays2);
         System.out.println("6.3.3-------------------------------------");
 
-        String string = reduction(stringList, (a) -> {
+        String string = reduce(stringList, (a) -> {
             String res = "";
             for (String s : a)
                 res += s;
@@ -87,7 +84,7 @@ public class Main {
         });
         System.out.println(string);
 
-        int sum = reduction(integerList, (a) -> {
+        int sum = reduce(integerList, (a) -> {
             int res = 0;
             for (int i : a)
                 res += i;
@@ -96,8 +93,8 @@ public class Main {
         System.out.println(sum);
 
 
-        int res = reduction(listOfArrays, new Reductiable<>(){
-        });
+        /*int res = reduction(listOfArrays, new Reductiable<>(){
+        });*/
 
 
     }
@@ -130,36 +127,35 @@ public class Main {
             list.add(i);
     }
 
-    public static <T, P> List<P> func(List<T> list, Appliable<T, P> a) {
+    public static <T, P> List<P> map(List<T> list, Applier<T, P> a) {
         List<P> res = new ArrayList<>();
         for (T el : list)
             res.add(a.apply(el));
         return res;
     }
 
-    public static <T> List<T> filter(List<T> list, Testable<T> a) {
+    public static <T> List<T> filter(List<T> list, Tester<T> a) {
         List<T> res = new ArrayList<>();
         for (T el : list)
             if(a.test(el))
                 res.add(el);
         return res;
-        Supplier
     }
 
-    public static <T> T reduction(List<T> list, Reductiable<T> a) {
+    public static <T> T reduce(List<T> list, Reductor<T> a) {
         return a.reduct(list);
     }
 }
 
-interface Appliable<T, P> {
+interface Applier<T, P> {
     P apply(T el);
 }
 
-interface Testable<T> {
+interface Tester<T> {
     boolean test(T el);
 }
 
-interface Reductiable<T> {
+interface Reductor<T> {
     T reduct(List<T> el);
 }
 
