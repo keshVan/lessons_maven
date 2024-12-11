877c0929b344e3cbbbc9eaab52e9224008eed329
keshVan <92533538+keshVan@users.noreply.github.com>
2024-12-11 13:04:25 +0400

    Add files via upload

--- src/ru/kornilaev/main/Main.java
+++ src/ru/kornilaev/main/Main.java
@@ -1,51 +1,25 @@
 package ru.kornilaev.main;
 
-import ru.kornilaev.generics.*;
-import ru.kornilaev.math.Fraction;
-import ru.kornilaev.geometry.*;
-
-import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import ru.kornilaev.animals.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Point> points = new ArrayList<>(List.of(new Point(1, 2), new Point(3, 4), new Point(5, 6)));
-        BrokenLine line = DataStream.of(points)
-                .filter((p) -> p.x > 1)
-                .map((p) -> p.x += 10)
-                .collect(BrokenLine::new, (l, p) -> l.addPoint(p));
-        System.out.println(line);
+        Cat cat = new Cat("барсик");
+        Dog dog = new Dog("крут");
+        MeowDog md = new MeowDog(dog);
+        MeowCount mw = new MeowCount(md);
+        test(mw);
+        System.out.println(mw.count);
     }
-
-    public static void shiftLine(Line<? extends Point> line) {
-
-        Point p = line.getStart();
-        if (p.x >= 0)
-            p.x += 10;
-        else
-            p.x -= 10;
-
-    }
-
-    public static double sumBoxes(Box<? extends Number>... boxes) {
-        double max = boxes[0].get().doubleValue();
-        for (int i = 1; i < boxes.length; i++) {
-            double value = boxes[i].get().doubleValue();
-            if (max < value) max = value;
-        }
-        return max;
-    }
-
-    public static void putPoint(Box box) {
-        box.set(new Point3D(1, 1, 1));
-    }
-
-    public static void fillList(List<Number> list) {
-        for (int i = 1; i < 101; i ++)
-            list.add(i);
+    public static void test(Meowable m) {
+        m.meow();
+        m.meow();
+        m.meow();
+        m.meow();
+        m.meow();
     }
 }
+
 
 
 
