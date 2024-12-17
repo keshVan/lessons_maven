8583830f501255839c4b683e3d0b122ecaa66fa9
keshVan <92533538+keshVan@users.noreply.github.com>
2024-12-17 15:20:33 +0400

    Add files via upload

--- src/ru/kornilaev/main/Main.java
+++ src/ru/kornilaev/main/Main.java
@@ -1,28 +1,14 @@
 package ru.kornilaev.main;
 
-import ru.kornilaev.animals.*;
+import ru.kornilaev.traffic_light.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("барсик");
-        Dog dog = new Dog("крут");
-        MeowDog md = new MeowDog(dog);
-        MeowCount mw = new MeowCount(md);
-        test(mw);
-        System.out.println(mw.count);
-    }
-    public static void test(Meowable m) {
-        m.meow();
-        m.meow();
-        m.meow();
-        m.meow();
-        m.meow();
+        TrafficLight tf = new TrafficLight(new Red());
+        tf.next();
+        tf.next();
+        tf.next();
+        tf.next();
+        tf.next();
     }
 }
-
-
-
-
-
-
-
